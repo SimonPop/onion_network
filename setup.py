38049d0bec68b8e20fe6b59fe8f8bca7a97863f5
Simon Popelier <simon.popelier@gmail.com>
2023-06-23 14:13:07 +0200

    :memo: Setup file updated to read requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,32 @@
-from setuptools import setup
+from setuptools import find_packages, setup
+import io
+import os
 
 with open("README.md", "r") as f:
     long_description = f.read()
+
+def read(*paths, **kwargs):
+    """Read the contents of a text file safely.
+    >>> read("project_name", "VERSION")
+    '0.1.0'
+    >>> read("README.md")
+    ...
+    """
+
+    content = ""
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *paths),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as open_file:
+        content = open_file.read().strip()
+    return content
+
+def read_requirements(path):
+    return [
+        line.strip()
+        for line in read(path).split("\n")
+        if not line.startswith(('"', "#", "-", "git+"))
+    ]
 
 setup(
     name="onion_network",
@@ -11,5 +36,5 @@ setup(
     author="Simon Popelier",
     author_email="simon.popelier@gmail.com",
     packages=["onion_network"],
-    install_requires=["wheel", "bar", "greek"],
+    install_requires=read_requirements("requirements.txt"),
 )
